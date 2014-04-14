dc2753cf77c79e043fbfef4c67ad2d8457da571c
guerillahunter <fiets_arnoud@live.nl>
2014-04-14 16:01:06 +0200

    building healt

---
+++ Assets/Health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float enemyHealth = 100f;
+
+	//void OnCollisionEnter2D(Collider2D other){
+
+		//}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "astroids")
+		{
+			Debug.Log("Enemy Hit!");
+			TakeDamage(50f);
+		}
+	}
+	void TakeDamage (float dmg)
+	{
+		enemyHealth -= dmg;
+		
+		if(enemyHealth <= 0)
+		{
+			EnemyDead();
+		}
+	}
+	
+	void EnemyDead()
+	{
+		Destroy(this.gameObject);
+	}
+	
+}
