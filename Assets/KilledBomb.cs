4bf6fc46499888a3f7684912c18a5cb668db8121
justin bieshaar <jscotty@hotmail.nl>
2014-04-16 12:07:16 +0200

    hallo arnoud

---
+++ Assets/KilledBomb.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KilledBomb : MonoBehaviour {
+
+	public Transform explosion;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "astroids")
+		{
+			Destroy (other.gameObject);
+			Instantiate(explosion, other.transform.position, other.transform.rotation);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
