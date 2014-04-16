cd1dc4a277fb55f885f37150af63e7bbbe4a444b
KitVanDeBunt <kitlvdbunt@hotmail.com>
2014-04-16 17:04:24 +0200

    stuff

--- Assets/playerCreator.cs
+++ Assets/playerCreator.cs
@@ -5,6 +5,6 @@ public class playerCreator : MonoBehaviour {
 
 	void Awake () {
 		GameObject player = GameObject.Instantiate(Menu.playerLoad,new Vector3(0,0,0),Quaternion.identity) as GameObject;
-		GameObject.Find("Camera").GetComponent<CameraFollow>().cameraTarget = player;
+		GameObject.Find("Camera").GetComponent<CameraFollow>().cameraTarget = GameObject.Find("aplayer");
 	}
 }
