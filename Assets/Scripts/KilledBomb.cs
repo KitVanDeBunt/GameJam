b5b1f150cac22c98faba1040ed322feb5c894df3
justin bieshaar <jscotty@hotmail.nl>
2014-04-16 15:01:45 +0200

    Justin game update

--- Assets/Scripts/KilledBomb.cs
+++ Assets/Scripts/KilledBomb.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class KilledBomb : MonoBehaviour {
 
 	public Transform explosion;
+
+	private string textScore;
+	private static float score = 0;
+	public GUIText scoreText;
+
+	void Awake(){
+		scoreText = GameObject.FindGameObjectWithTag ("ScoreTextUI").GetComponent<GUIText> ();
+		textScore = score.ToString ();
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.gameObject.tag == "astroids")
 		{
 			Destroy (other.gameObject);
 			Instantiate(explosion, other.transform.position, other.transform.rotation);
+			score += 5;
 		}
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		scoreText.text = "SCORE = " + score;
 	}
 }
